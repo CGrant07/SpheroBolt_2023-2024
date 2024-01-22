4eec61a3bbf9e8534e52c55ca69eae7d20af95d8
Grant <carsongrant778@gmail.com>
2024-01-22 08:41:08 -0500

    Blue Circle + (1.22.2024)
    Sphere to blue circle +turn 90 to next checkpoint

--- Maze.js
+++ Maze.js
@@ -4,6 +4,8 @@
 //Program: Sphero Bolt Maze
 //Bolt ID: B2F8
 
+//Start Commit
+
 async function startProgram() {
 
 	setMainLed({ r: 255, g: 255, b: 255 });
@@ -11,6 +13,16 @@ async function startProgram() {
 	await speak("Start", true); 
 
 	await scrollMatrixText('Start!', { r: 66, g: 56, b: 255 }, 15, true)
+	
+	//Blue Sphere
+	
+	await roll((getHeading() + 0), 60, 2.5); 
+	
+	await setMainLed({ r: 0, g: 0, b: 255 }); 
+	
+	await delay (1);
+	
+	await roll((getHeading() + 90), 60, 1.5); 
 
 
 
