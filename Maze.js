442553550db6952cb1223d9f8308c69f6ff34658
Grant <carsongrant778@gmail.com>
2024-01-22 08:27:54 -0500

    Start! and Print Text

--- Maze.js
+++ Maze.js
@@ -4,3 +4,14 @@
 //Program: Sphero Bolt Maze
 //Bolt ID: B2F8
 
+async function startProgram() {
+
+	setMainLed({ r: 255, g: 255, b: 255 });
+
+	await speak("Start", true); 
+
+	await scrollMatrixText('Start!', { r: 66, g: 56, b: 255 }, 15, true)
+
+
+
+}
